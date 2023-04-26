3a1d6b54969a875f595f341966085f0256351536
NagendraBabuG <gnagendra0009@gmail.com>
2023-04-26 08:47:40 +0530

    added ioredis and upstash redis

---
+++ clientRedis.js
@@ -0,0 +1,3 @@
+const Redis = require('ioredis')
+const clientRedis = new Redis('redis://default:7814d5bd505b45c0927593a8bd16ce12@us1-helping-mackerel-38790.upstash.io:38790')
+module.exports = clientRedis
--- routes/getData.js
+++ routes/getData.js
@@ -4,68 +4,126 @@ const mongoose = require('mongoose')
 const userModel = require('../model/user')
 
 const router = express.Router()
-const middleware = require('../middleware/index')
-router.use(middleware)
+const clientRedis = require('../clientRedis')
+const middlewareCache = async (req, res, next) => {
+  const { key } = req.params;
+  if (key === undefined) {
+    next();
+  } else {
+    try {
+      const data = await redisClient.get(key);
+      if (data !== null) {
+        console.log('using redis');
+        return res.status(200).json({ status: 'success', data: JSON.parse(data) });
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.log(err);
+      next();
+    }
+  }
+}
+
 // Users which have income lower than $5 USD and have a car of brand “BMW” or “Mercedes”.
-router.get('/query1', async (req, res)=> {
+router.get('/query1',middlewareCache, async (req, res)=> {
+  try{
+  
     const users = await userModel.find({
         income: { $lt: 5 }, 
         car: { $in: ['BMW', 'Mercedes'] }
       });
+      clientRedis.set('query1', JSON.stringify(users))
       console.log(users.length)
       return res.status(200).json({status : 'success', data : users})
-})
+    
+  }
+  catch(error)
+  {
+    return res.status(404).json({status: 'error', data : error})
+}})
+
 // . Male Users which have phone price greater than 10,000.
-router.get('/query2', async (req, res)=> {
+router.get('/query2', middlewareCache, async (req, res)=> {
+  try{
     const maleUsersWithPhoneG10K = await userModel.find({
-        gender: 'Male', // Filter for gender "Male"
-        phonePrice: { $gt: 10000 } // Filter for phone price greater than $10,000
-      })
-    console.log(maleUsersWithPhoneG10K.length)
-    return res.status(200).json({status: 'success', data: maleUsersWithPhoneG10K})
+      gender: 'Male', // Filter for gender "Male"
+      phonePrice: { $gt: 10000 } // Filter for phone price greater than $10,000
+    })
+  console.log(maleUsersWithPhoneG10K.length)
+  clientRedis.set('query2', JSON.stringify(maleUsersWithPhoneG10K))
+  return res.status(200).json({status: 'success', data: maleUsersWithPhoneG10K})
+  }
+  catch(error)
+  {
+    return res.status(404).json({status: 'error', data : 'error'})
+  }
+    
 })
 
 // Users whose last name starts with “M” and has a quote character length greater than 15 and email includes his/her last name.
-router.get('/query3', async (req, res)=> {
+router.get('/query3',middlewareCache, async (req, res)=> {
+  try{
     const queryResponse = await userModel.find({
       last_name: { $regex: /^M/i }, // Starts with "M", case-insensitive
       $expr: { $gt: [{ $strLenCP: "$quote" }, 15] }, // Quote character length greater than 15
       email: { $regex: /M/i } // Email includes last name, case-insensitive
     });
+    clientRedis.set('query3', JSON.stringify(queryResponse))
     console.log(queryResponse.length)
     return res.status(200).json({status: 'success', data: queryResponse})
+  }
+  catch(error)
+  {
+    return res.status(404).json({status: 'error', data : 'error'})
+  }
 })
 
 
 //Users which have a car of brand “BMW”, “Mercedes” or “Audi” and whose email does not include any digit.
-router.get('/query4', async (req, res)  => {
+router.get('/query4',middlewareCache, async (req, res)  => {
+  try{
+    const result = await userModel.find({
+      car: { $in: ["BMW", "Mercedes", "Audi"] },
+      email: { $not: { $regex: /\d/ } } 
+    })
+    clientRedis.set('query4', JSON.stringify(result))
+    console.log(result.length)
+    return res.status(400).json({status: 'success', data : result})
+  }
+  catch(error)
+  {
+    return res.status(404).json({status: 'error', data : 'error'})
+  }
   
-  const result = await userModel.find({
-    car: { $in: ["BMW", "Mercedes", "Audi"] }, // Car brand is "BMW", "Mercedes", or "Audi"
-    email: { $not: { $regex: /\d/ } } // Email does not include any digit
-  })
-  console.log(result.length)
-  return res.status(400).json({status: 'success', data : result})
 })
 //Show the data of top 10 cities which have the highest number of users and their average income.
-router.get('/query5', async (req, res) => {
-  const top10Cities = await userModel.aggregate([
-    {
-      $group: {
-        _id: "$city", // Group by city
-        userCount: { $sum: 1 }, // Count the number of users in each city
-        avgIncome: { $avg: "$income" } // Calculate the average income in each city
+router.get('/query5',middlewareCache, async (req, res) => {
+  try{
+    const top10Cities = await userModel.aggregate([
+      {
+        $group: {
+          _id: "$city", // Group by city
+          userCount: { $sum: 1 }, // Count the number of users in each city
+          avgIncome: { $avg: "$income" } // Calculate the average income in each city
+        }
+      },
+      {
+        $sort: { userCount: -1 } // Sort by userCount in descending order
+      },
+      {
+        $limit: 10 // Limit the result to top 10 cities
       }
-    },
-    {
-      $sort: { userCount: -1 } // Sort by userCount in descending order
-    },
-    {
-      $limit: 10 // Limit the result to top 10 cities
-    }
-  ])
-  console.log(top10Cities.length)
-  return res.status(200).json({status : 'success', data : top10Cities})
-})
+    ])
+    clientRedis.set('query5', JSON.stringify(top10Cities))
+    console.log(top10Cities.length)
+    return res.status(200).json({status : 'success', data : top10Cities})
+  }
+  catch(error)
+  {
+    return res.status(404).json({status: 'error', data : 'error'})
+  }
+ 
+  })
 
 module.exports = router
